e0f79def59de8456165efc9f4a01e474637cd964
Nick Schilders <90120063+ComradeBlyatlov@users.noreply.github.com>
2021-12-23 18:32:19 -0500

    Update Refinery.py

--- Refinery.py
+++ Refinery.py
@@ -39,7 +39,7 @@ for x in tablelist:
         #Before
 
         print(y)
-        if y[5]=='2021-07-12' and (y[4] != 'Error') and (y[3] != 'Error') and(y[3]!='>-0.01') and (int(y[2])>10):
+        if y[5]=='2021-07-12' and (y[4] != 'Error') and (y[3] != 'Error') and(y[3]!='>-0.01') and (int(y[2])>1):
             print(x,' ',y[4], ' ', y[3])
             ticker1.append(x)
             ticker1.append(y[4])
